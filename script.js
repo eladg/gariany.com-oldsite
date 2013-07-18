3b1ed00b5f80c5d5762803468ed781cb11173669
eladg <ggrelad@gmail.com>
2013-07-19 02:47:09 +0300

    fix double animation issue

--- script.js
+++ script.js
@@ -1,48 +1,67 @@
 var currentlyOpened = 'content_about_div';
-var px_open = '700px';
+var px_open = '690px';
 var px_close = '190px';
 
+var isAnimating;
+function setAnimating(state) {
+    if (state === true) {
+        isAnimating = true;
+    } else {
+        isAnimating = false;
+    }
+}
+
 function togelOpen(sender) {
-    if (sender == currentlyOpened) {return;}
+    if (sender == currentlyOpened) { return; }
+    if (isAnimating === true) { return; }
+
+    // text selectors
+    setAnimating(true);
+    console.log("setAnimating(true) = " + isAnimating);
 
     var currentContentTextSelector = '.' + currentlyOpened + ' > ' + '.content_text';
     var senderContentTextSelector = '.' + sender + ' > ' + '.content_text';
 
-    // close the currently open
-    $(currentContentTextSelector).fadeOut(200);
-    $('.' + currentlyOpened).animate({
-        width: px_close
-    });
-
     // open the selected one
-    $('.' + sender).animate(
-        {
-            width: px_open
-        },
-        400,
-        function() {
-            $(senderContentTextSelector).fadeIn(200);
-        }
+    var openAnimation = function() {
+        $('.' + sender).animate(
+            { width: px_open },
+            300,
+            function() {
+                $(senderContentTextSelector).fadeIn(200);
+                setAnimating(false);
+            }
+        );
+    };
+
+
+    // close the currently open
+    $(currentContentTextSelector).css(
+        { display: "none"}
+    );
+    $('.' + currentlyOpened).animate(
+        {width: px_close},
+        300,
+        openAnimation
     );
 
     currentlyOpened = sender;
+    console.log("setAnimating(false) = " + isAnimating);
 }
 
 $(document).ready(function() {
 
-    $('.content_about_div').fadeIn(1000);
-
-    $('.content_about_div').click(function() {
+    $('.content_about_div').mouseover(function() {
         console.log("mouseover content_about_div");
         togelOpen('content_about_div');
     });
 
-    $('.content_tech_div').click(function() {
+    $('.content_tech_div').mouseover(function() {
         console.log("mouseover content_tech_div");
         togelOpen('content_tech_div');
     });
 
-    $('.content_arts_div').click(function() {
+    $('.content_arts_div').mouseover(function() {
         console.log("mouseover content_arts_div");
         togelOpen('content_arts_div');
     });
