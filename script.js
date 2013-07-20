749efac4b2ed018f8624cbb896ab31d5782288ea
eladg <ggrelad@gmail.com>
2013-07-20 17:23:28 +0300

    fix animations for sections

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var currentlyOpened = 'content_about_div';
+var currentlyOpened = 'content-about-div';
 var px_open = '690px';
-var px_close = '190px';
+var px_close = '167px';
 
 var isAnimating;
 function setAnimating(state) {
@@ -11,57 +11,103 @@ function setAnimating(state) {
     }
 }
 
-function togelOpen(sender) {
-    if (sender == currentlyOpened) { return; }
-    if (isAnimating === true) { return; }
+function loadSectionDetails(section) {
 
-    // text selectors
+    // ==========================================================
+    // selectors:
+    // ==========================================================
+
+    // text text
+    var contentDetailsDivSelector   = '.' + section + ' > .content-details';
+
+    // ==========================================================
+    // callbacks:
+    // ==========================================================
+    var finishedAnimationBlock = function() {
+        setAnimating(false);
+    };
+
+    $(contentDetailsDivSelector).animate(
+        { opacity: 1},
+        200,
+        finishedAnimationBlock
+    );
+
+}
+
+function toggleOpenSection(sender) {
+    if (sender == currentlyOpened) {
+        console.log("Already opened...");
+        return;
+    }
+    if (isAnimating === true) {
+        console.log("Still animating...");
+        return;
+    }
+
+    // Prevent 2 animation at the same time
     setAnimating(true);
-    console.log("setAnimating(true) = " + isAnimating);
 
-    var currentContentTextSelector = '.' + currentlyOpened + ' > ' + '.content_text';
-    var senderContentTextSelector = '.' + sender + ' > ' + '.content_text';
+    // ==========================================================
+    // selectors:
+    // ==========================================================
+    // opened/close divs
+    var divCurrentlyOpenSelector      = '.' + currentlyOpened;
+    var divSenderSelector             = '.' + sender;
 
-    // close the currently open
-    $(currentContentTextSelector).css(
-        { display: "none"}
+    // h2 title
+    var titleCurrentlyOpenedSelector  = '.' + currentlyOpened + ' > h2';
+    // 
+    var textSection   = '.' + currentlyOpened + ' > .content-details';
+
+    // ==========================================================
+    // callbacks:
+    // ==========================================================
+    var finishedAnimatingBlock = function() {
+        currentlyOpened = sender;
+        loadSectionDetails(sender);
+    };
+
+    var toggelSectionAnimationBlock = function() {
+        console.log("set width of " + divCurrentlyOpenSelector + " to " + px_close);
+        $(divCurrentlyOpenSelector).animate(
+            { width: px_close },
+            300
+        );
+
+        console.log("set width of " + divSenderSelector + " to " + px_open);
+        $(divSenderSelector).animate(
+            { width: px_open },
+            300,
+            finishedAnimatingBlock
+        );
+    };
+
+    // ==========================================================
+    // animations:
+    // ==========================================================
+    px_close = $(titleCurrentlyOpenedSelector).css('width');
+
+    // make current text disapear
+    $(textSection).animate(
+        { opacity: 0},
+        200,
+        toggelSectionAnimationBlock
     );
-
-    // close animation
-    $('.' + currentlyOpened).animate(
-        {width: px_close},
-        300
-    );
-
-    // open animation
-    $('.' + sender).animate(
-        { width: px_open },
-        300,
-        function() {
-            $(senderContentTextSelector).fadeIn(200);
-            setAnimating(false);
-        }
-    );
-
-    currentlyOpened = sender;
-    console.log("setAnimating(false) = " + isAnimating);
 }
 
 $(document).ready(function() {
 
-    $('.content_about_div').mouseover(function() {
-        console.log("mouseover content_about_div");
-        togelOpen('content_about_div');
+    $('.content-about-div').mouseover(function() {
+        toggleOpenSection('content-about-div');
     });
 
-    $('.content_tech_div').mouseover(function() {
-        console.log("mouseover content_tech_div");
-        togelOpen('content_tech_div');
+    $('.content-tech-div').mouseover(function() {
+        toggleOpenSection('content-tech-div');
     });
 
-    $('.content_arts_div').mouseover(function() {
-        console.log("mouseover content_arts_div");
-        togelOpen('content_arts_div');
+    $('.content-arts-div').mouseover(function() {
+        toggleOpenSection('content-arts-div');
     });
 
 });
