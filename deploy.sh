df748189c040f70bf6137642cff931d440c75956
eladg <ggrelad@gmail.com>
2013-10-10 20:26:10 -0400

    stupid deploy script: done

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,20 @@
 # test everything:
 type s3cmd >/dev/null 2>&1 || { echo >&2 "s3cmd is not installed. Aborting."; exit 1; }
 
-echo "cloning repository to /tmp/gariany.com"
+# Start here:
 rm -rf /tmp/gariany.com
+
+# clone repository 
+echo "cloning repository to /tmp/gariany.com"
 git clone https://github.com/eladg/gariany.com.git /tmp/gariany.com
-rm -rf /tmp/gariany.com/.git
 echo ""
+
+# remove files that should not be on 
+echo "removing unnecessary web files"
+rm -rf /tmp/gariany.com/.git
+rm -rf /tmp/gariany.com/deploy.sh
+echo "done."
+echo "" 
 
 echo "uploading to S3"
 s3cmd sync /tmp/gariany.com/ s3://gariany.com/
