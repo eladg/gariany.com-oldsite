43a1a8396b065adb34e1cd84c30524223332bd09
eladg <ggrelad@gmail.com>
2013-10-31 17:26:42 -0700

    update tech,arts section

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var currentlyOpened = 'content-about-div';
+var currentlyOpened = 'about-main';
 var currentBackgroundImage = 3;
 var px_open = '54%';
 var px_close = '1px';
@@ -15,8 +15,8 @@ function setAnimating(state) {
 function setDivsWidth() {
 
     // I'm well aware that tms is really wrong, but well, i'm still a total HTML/CSS/JS newbie
-    var techDivSelector = ".content-tech-div";
-    var artsDivSelector = ".content-arts-div";
+    var techDivSelector = ".tech-main";
+    var artsDivSelector = ".arts-main";
 
     var techTitleSelector = techDivSelector + " > h2";
     var artsTitleSelector = artsDivSelector + " > h2";
@@ -24,52 +24,40 @@ function setDivsWidth() {
     px_title_tech = $(techTitleSelector).css('width');
     px_title_arts = $(artsTitleSelector).css('width');
 
-    console.log("width px_title_tech: " + px_title_tech);
-    console.log("width px_title_arts: " + px_title_arts);
+    // console.log("width px_title_tech: " + px_title_tech);
+    // console.log("width px_title_arts: " + px_title_arts);
 
     $(techDivSelector).css('width', px_title_tech);
     $(artsDivSelector).css('width', px_title_arts);
 
 }
 
-function animateBackground() {
-    // var currentBackgroundSelector = '.dynamic-background-' + currentBackgroundImage;
-    // $(currentBackgroundSelector).animate(
-    //     { marginTop: -500},
-    //     60000,
-    //     "linear"
-    // );
-}
-
 function changeBackground() {
-    console.log("in changeBackground");
+    // console.log("in changeBackground");
     var backgroundSelector = '.dynamic-background-' + currentBackgroundImage;
 
     $(backgroundSelector).stop();
-    console.log("backgroundSelector = " + backgroundSelector);
+    // console.log("backgroundSelector = " + backgroundSelector);
     // set current backgorund opacity to 0
     $(backgroundSelector).animate(
         { opacity: 0},
         1500
     );
 
-    if (currentlyOpened == 'content-about-div') {
+    if (currentlyOpened == 'about-main') {
         currentBackgroundImage = 3;
-    } else if (currentlyOpened == 'content-tech-div') {
+    } else if (currentlyOpened == 'tech-main') {
         currentBackgroundImage = 1;
-    } else if (currentlyOpened == 'content-arts-div' ) {
+    } else if (currentlyOpened == 'arts-main' ) {
         currentBackgroundImage = 2;
     }
 
     // set current backgorund opacity to 1
     var backgroundSelector = '.dynamic-background-' + currentBackgroundImage;
-    console.log("backgroundSelector = " + backgroundSelector);
+    // console.log("backgroundSelector = " + backgroundSelector);
     $(backgroundSelector).animate(
         { opacity: 1, zindex: 0},
-        1500,
-        function() {
-            animateBackground();
-        }
+        1500
     );
 
 }
@@ -81,7 +69,7 @@ function loadSectionDetails(section) {
     // ==========================================================
 
     // text text
-    var contentDetailsDivSelector   = '.' + section + ' > .content-details';
+    var contentDetailsDivSelector   = '.' + section + ' > .text-block';
 
     // ==========================================================
     // callbacks:
@@ -100,17 +88,17 @@ function loadSectionDetails(section) {
 }
 
 function aboutIconAnimationEnded(sender) {
-    console.log("in aboutIconAnimationEnded");
-    console.log(sender);
+    // console.log("in aboutIconAnimationEnded");
+    // console.log(sender);
 }
 
 function toggleOpenSection(sender) {
     if (sender == currentlyOpened) {
-        console.log("Already opened...");
+        // console.log("Already opened...");
         return;
     }
     if (isAnimating === true) {
-        console.log("Still animating...");
+        // console.log("Still animating...");
         return;
     }
 
@@ -127,7 +115,7 @@ function toggleOpenSection(sender) {
     // h2 title
     var titleCurrentlyOpenedSelector  = '.' + currentlyOpened + ' > h2';
     // 
-    var textSection   = '.' + currentlyOpened + ' > .content-details';
+    var textSection   = '.' + currentlyOpened + ' > .text-block';
 
     // ==========================================================
     // callbacks:
@@ -138,13 +126,13 @@ function toggleOpenSection(sender) {
     };
 
     var toggelSectionAnimationBlock = function() {
-        console.log("set width of " + divCurrentlyOpenSelector + " to " + px_close);
+        // console.log("set width of " + divCurrentlyOpenSelector + " to " + px_close);
         $(divCurrentlyOpenSelector).animate(
             { width: px_close },
             300
         );
 
-        console.log("set width of " + divSenderSelector + " to " + px_open);
+        // console.log("set width of " + divSenderSelector + " to " + px_open);
         $(divSenderSelector).animate(
             { width: px_open },
             300,
@@ -167,25 +155,18 @@ function toggleOpenSection(sender) {
 
 $(document).ready(function() {
 
-    animateBackground();
     setDivsWidth();
 
-    // debug:
-    $('.content-top > h1').mouseover(function() {
-        console.log('in mouseover');
-        toggleOpenSection('content-tech-div');
+    $('.about-main').mouseover(function() {
+        toggleOpenSection('about-main');
     });
 
-    $('.content-about-div').mouseover(function() {
-        toggleOpenSection('content-about-div');
+    $('.tech-main').mouseover(function() {
+        toggleOpenSection('tech-main');
     });
 
-    $('.content-tech-div').mouseover(function() {
-        toggleOpenSection('content-tech-div');
-    });
-
-    $('.content-arts-div').mouseover(function() {
-        toggleOpenSection('content-arts-div');
+    $('.arts-main').mouseover(function() {
+        toggleOpenSection('arts-main');
     });
 
 });
